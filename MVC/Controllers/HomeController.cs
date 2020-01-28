ce422163c6494cf405f661c2c9fb3e0e572a22a4
MaxRivas <anzor13@hotmail.com>
2020-01-28 11:13:04 -0700

    removed intial quote from solution name

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using _MVC.Models;
+using MVC.Models;
 
-namespace _MVC.Controllers
+namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
--- MVC/Models/ErrorViewModel.cs
+++ MVC/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace _MVC.Models
+namespace MVC.Models
 {
     public class ErrorViewModel
     {
