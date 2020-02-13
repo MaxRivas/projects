eeafbbfa2915e3dc3add06762452612785a3723c
MaxRivas <anzor13@hotmail.com>
2020-02-13 11:53:49 -0700

    time travel implemented

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -61,10 +61,10 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    
+        
     if (calculateWinner(squares) || squares[i]){
       return;
     }
@@ -73,6 +73,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{squares: squares,}]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
 
@@ -86,7 +87,7 @@ class Game extends React.Component {
   render() {
 
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step,move) => {
